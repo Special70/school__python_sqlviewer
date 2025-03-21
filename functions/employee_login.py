83f1b4764f669440631d98f1f2beab8e86a73732
vbenrey <155866494+vbenrey@users.noreply.github.com>
2025-03-21 19:37:18 +0800

    Add files via upload
    employee login

---
+++ functions/employee_login.py
@@ -0,0 +1,18 @@
+from standalone.global_vars import cursor, connection
+from standalone.functions.exist_check import exist_check
+from functions.exiting import exiting
+import time
+
+def employee_login():
+    employee_name = input("Enter Employee Name [Firstname Lastname]: ")
+    login = exist_check("employees", "employee_name", employee_name)
+    if login == 0:
+        print(f"{employee_name} is not a registered employee. Exiting the system per protocol.\n")
+        exiting(1)
+        
+    else:
+        print(f"Logging in as {employee_name}..")
+        time.sleep(2.5)
+    cursor.execute("select employee_id from employees where employee_name = ?", (employee_name,))
+    employee_id = cursor.fetchone()[0]
+    return employee_id
